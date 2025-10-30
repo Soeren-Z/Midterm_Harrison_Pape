3e8e664adfab1ca0890116e9fe573ce8564b376e
Harrison Pape <harrison.pape@georgebrown.ca>
2025-10-30 10:39:45 -0400

    Multiplication lsit logic

--- app/src/main/java/com/example/midterm_harrison_pape/MainActivity.java
+++ app/src/main/java/com/example/midterm_harrison_pape/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.midterm_harrison_pape;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputNum;
+    private Button submit;
+    private ListView multiplicationList;
+    private Button history;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        inputNum = findViewById(R.id.inputNum);
+        submit = findViewById(R.id.submit);
+        multiplicationList = findViewById(R.id.multiplicationList);
+        history = findViewById(R.id.history);
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        multiplicationList.setAdapter(adapter);
+
+        multiplicationList.setOnItemClickListener((parent, view, position, id) -> {
+            String item = list.get(position);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder
+                    .setTitle("Delete Row")
+                    .setMessage("Do you want to delete this row?" + item)
+                    .setNegativeButton("Cancel", null)
+                    .setPositiveButton("Okay", (dialog, w) -> {
+                        list.remove(position);
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(this, "Deleted row: " + item, Toast.LENGTH_SHORT).show();
+                    })
+                    .show();
+        });
+    }
+    public void GenerateList(View view) {
+        String input = inputNum.getText().toString().trim();
+        if(input.isEmpty()) {
+            Toast.makeText(this, "Enter a number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        int num = Integer.parseInt(input);
+        list.clear();
+        for(int i = 1; i < 11; i++) {
+            list.add(num + " x " + i + " = " + (num * i));
+        }
+        adapter.notifyDataSetChanged();
     }
 }
